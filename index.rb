db28c59a7f10681dff8ada4409c1b65d3f26cf92
Ale Prieto <aleprieto@gmail.com>
2014-08-21 21:52:37 -0300

    Clase mail

--- index.rb
+++ index.rb
@@ -1,6 +1,9 @@
+# encoding: utf-8
 require 'csv'
 
-archivo = 'inscripciones_ASISTENTES.csv' 
+archivo = 'inscripciones_ASISTENTES.csv'
+certificados_no_existentes = 0
+faltantes = []
 
 # CSV.open(archivo, 'r', ';') do |row|
 #   puts row
@@ -12,6 +15,25 @@ CSV.foreach(archivo, quote_char: '"', col_sep: ',', row_sep: :auto, headers: fal
   correo = row[2]
   # usuario = correo.split('@')[0]
   usuario = nombre.downcase.tr(' ','') + apellido.downcase.tr(' ', '')
-  certificado = 'Certificado-' + usuario + '.pdf'
-  puts usuario + ' :: ' + certificado
+  certificado = "Certificado-#{usuario}.pdf"
+
+  if File.file?('certificados/' + certificado)
+  	puts "#{usuario} :: #{certificado} :: ¿existe? " + (File.file?('certificados/' + certificado)).to_s
+  else
+  	certificados_no_existentes += 1
+  	faltantes.push("#{apellido}, #{nombre}")
+  end
 end
+
+# Certificados faltantes
+if certificados_no_existentes > 0
+	faltantes.each do|f|
+		puts "Falta: #{f}"
+	end 
+	
+	puts "\nCertificados no existentes: #{certificados_no_existentes}"
+else
+	puts "\nTodos los certificados fueron encontrados :)"
+end
+
+puts ""
